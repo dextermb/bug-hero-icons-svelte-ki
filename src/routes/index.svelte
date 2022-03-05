f65de236cb8dfb1642ee3cde6187aa0eb2a089ae
Dexter Marks-Barber <dexter@marks-barber.co.uk>
2022-03-05 18:30:53 +0000

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { Icon, Home } from 'svelte-heroicons';
+	import '../tailwind.css';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Icon src={Home} class="w-5 h-5" solid />
